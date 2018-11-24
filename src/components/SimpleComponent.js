cd8fd5a663fe2d6f9a75bdcccc27cb430b350cd5
zhifeng ou <zhifengou@hotmail.com>
2018-11-24 23:23:39 +0000

    automatically backed up by learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,15 @@
 // Code SimpleComponent Here
+import React, { Component } from 'react';
+
+export default class SimpleComponent extends Component {
+  state={ mood:'happy' }
+
+  handleClick = () => {
+    const newMood = this.state.mood === 'happy' ? 'sad' : 'happy';
+    this.setState({ mood: newMood });
+  }
+
+  render() {
+    return <div onClick={this.handleClick}>{this.state.mood}</div>;
+  }
+}
--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,6 @@
 // Code SimplerComponent Here
+import React from 'react';
+
+const SimplerComponent = ()=>{
+  <div onClick={props.handleClick}>I am just happy.</div>
+}
